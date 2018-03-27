23e8f20391e3bf88c8d6339ea67ee95edb095269
sergda <sergda@silversite.ru>
2018-03-27 17:46:32 +0300

    слайдер adminka js

--- public/packages/barryvdh/elfinder/js/standalonepopup.js
+++ public/packages/barryvdh/elfinder/js/standalonepopup.js
@@ -14,6 +14,29 @@ $(document).on('click','.popup_selector',function (event) {
 });
 // function to update the file selected by elfinder
 function processSelectedFile(filePath, requestingField) {
-    $('#' + requestingField).val("/"+filePath.replace("\\", "/") ).trigger('change');
-    $('.' + requestingField).attr("src", "/"+filePath.replace("\\", "/")).trigger('change');
+    if(requestingField == "en_slidexBox" || requestingField == "fr_slidexBox" || requestingField == "de_slidexBox"){
+
+        var blockSlide = $('.' + requestingField).parents("div.sliderBlock");
+        var description = $(blockSlide).find('.descriptionSlide').val();
+
+        $('.' + requestingField).append(
+            "<div class='col-md-6 imgBlock'>"+
+            "<div class='deleteImageSlider'>&times;</div>"+
+            "<div style='text-align: center;'>"+description+"</div>"+
+            "<img src='/"+ filePath.replace("\\", "/") +"' alt='"+description+"' style='width: 150px; height: 150px;'/>"+
+            "</div>"
+        );
+        $(blockSlide).find('.descriptionSlide').val('');
+        var blockImages = $('.' + requestingField + ' img');
+        var jsonInput = [];
+        blockImages.each(function(){
+            var ob = {src:$(this).attr('src'),alt:$(this).attr('alt')};
+            jsonInput.push(ob);
+        });
+        $(blockSlide).find('.slide_input').val(encodeURIComponent(JSON.stringify(jsonInput)));
+
+    }else {
+        $('#' + requestingField).val("/" + filePath.replace("\\", "/")).trigger('change');
+        $('.' + requestingField).attr("src", "/" + filePath.replace("\\", "/")).trigger('change');
+    }
 }
